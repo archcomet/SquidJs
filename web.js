da0733086f3562ff4c453c9b6f36653b06402761
archcomet <archcomet@gmail.com>
2013-02-10 15:21:52 -0800

    - Fixed error handling on 404

--- web.js
+++ web.js
@@ -16,14 +16,12 @@
             file.serve(req, res, function (err, result) {
                 if (err) {
                     console.error('Error serving %s - %s', req.url, err.message);
-                    if (err.status === 404 || err.status === 500) {
-                        file.serveFile(util.format('/%d.html', err.status), err.status, {}, req, res);
-                    } else {
+                    if (err.status !== 404 && err.status !== 500) {
                         res.writeHead(err.status, err.headers);
                         res.end();
                     }
                 } else {
-                    console.log('%s - %s', req.url, res.message);
+                    console.log('%s', req.url);
                 }
             });
         });
