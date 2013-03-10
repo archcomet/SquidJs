aaf128e1a6a120c44f56bc6712bcc6b037ec79b2
archcomet <archcomet@gmail.com>
2013-03-10 16:26:37 -0700

    - Fixed issue causing crash with node express

--- web.js
+++ web.js
@@ -11,12 +11,10 @@
         mongoClient = require('mongodb').MongoClient,
         url = require('url');
 
-    app = express();
-
-    app.configure(function () {
-        app.use(express.bodyParser());
-        app.use(express['static'](__dirname));
-    });
+    app = express.createServer(
+        express['static'](__dirname),
+        express.bodyParser()
+    );
 
     app.configure('development', function () {
         app.use(express.errorHandler({ dumpExceptions: true, showStack: true }));
